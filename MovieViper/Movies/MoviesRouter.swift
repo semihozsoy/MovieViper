cece834bdfc3f66373b9002e9fcac66f5f6e7f9c
Semih Özsoy <semih__ozsoy@hotmail.com>
2024-03-16 18:45:40 +0300

    movies scene implemented

---
+++ MovieViper/Movies/MoviesRouter.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesRouter.swift
+//  MovieViper
+//
+//  Created by Semih Ozsoy on 14.03.2024.
+//
+
+import Foundation
---
+++ MovieViper/Movies/View/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  MovieViper
+//
+//  Created by Semih Ozsoy on 14.03.2024.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
