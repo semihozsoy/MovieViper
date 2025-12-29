d158e8b6c41cad5d63219a1b6623ed67f79714a1
Semih Özsoy <semih__ozsoy@hotmail.com>
2025-12-29 23:46:55 +0300

    feat(MOV: 000): unit test init

---
+++ MovieViperTests/MovieViperTests.swift
@@ -0,0 +1,21 @@
+//
+//  MovieViperTests.swift
+//  MovieViperTests
+//
+//  Created by Semih Özsoy on 29.12.2025.
+//
+
+import XCTest
+
+final class MovieViperTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+
+    func testPerformanceExample() throws {
+        measure {}
+    }
+}
